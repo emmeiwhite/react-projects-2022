f9ecbbdd17522083af04d99ffc9db1f5f6431142
Imran Rafiq Rather <emmeiwhite@gmail.com>
2023-01-03 10:16:57 +0530

    emmei: made use of index state and used that index in each object

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -5,6 +5,31 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { id, name, job, image, text } = people[index];
+
+  const handlePrev = () => {
+    if (index <= 0) {
+      setIndex(people.length - 1);
+    } else {
+      setIndex((prevIndex) => {
+        return prevIndex - 1;
+      });
+    }
+  };
+
+  const handleNext = () => {
+    if (index >= people.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex((prevIndex) => {
+        return prevIndex + 1;
+      });
+    }
+  };
+
+  const handleRandom = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    setIndex(randomNumber);
+  };
   return (
     <article className="review">
       <div className="img-container">
@@ -18,16 +43,28 @@ const Review = () => {
       <p className="job">{job}</p>
       <p className="info">{text}</p>
       <div className="button-container">
-        <button className="prev-btn">
+        <button
+          className="prev-btn"
+          onClick={() => {
+            handlePrev();
+          }}
+        >
           <FaChevronLeft />
         </button>
 
-        <button className="next-btn">
+        <button
+          className="next-btn"
+          onClick={() => {
+            handleNext();
+          }}
+        >
           <FaChevronRight />
         </button>
       </div>
 
-      <button className="random-btn">surprise me</button>
+      <button className="random-btn" onClick={handleRandom}>
+        surprise me
+      </button>
     </article>
   );
 };
